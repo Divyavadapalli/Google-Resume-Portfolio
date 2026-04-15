378d42b07de98c0b4fa6082561efaf2c234c42a9
divya sri <vadapallidivyasrii@gmail.com>
2026-04-15 18:37:39 +0530

    fix netlify build

---
+++ open-browser.ps1
@@ -0,0 +1,3 @@
+# Wait for server to start, then open browser
+Start-Sleep -Seconds 3
+Start-Process "http://localhost:3000/"
---
+++ start.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Starting Resume Portfolio...
+cd /d "%~dp0"
+npm run dev
+pause
